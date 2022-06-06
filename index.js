196dafc212c9600880b2bddacda474412dfb5487
Zacharia <ndunguzachary24@gmail.com>
2022-06-06 21:42:59 +0300

    commit tree 9b7e3f90227b977dd9f13ad1a439f54d35e07443

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if( distance <= 400){
+    return "This one is on me!"
+  }else if(distance > 2000){
+    return 'I will gladly take your thirty bucks.'
+  }  else if (distance  >  2500){
+    return  'No can do.'
+  }  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+//  return  city === 'NYC'  ? "Ok, sounds good." 
 }
 
 function switchOnCharmFromTip(){
